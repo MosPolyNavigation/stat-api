8a948b3041cf252669f909a8b6b315895f178245
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-12-29 23:10:25 +0300

    Update methods

--- methods.py
+++ methods.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
-from fastapi.responses import JSONResponse
-import models, schemas
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import Select
+import models
+import schemas
+
 
 async def create_uuid(db: Session) -> schemas.UUID:
     item = models.UUID()
@@ -9,11 +12,13 @@ async def create_uuid(db: Session) -> schemas.UUID:
     db.refresh(item)
     return item
 
-async def insert_site_stat(db: Session, data: schemas.SiteStat) -> JSONResponse:
-    item = models.SiteStat(uuid=data.uuid, endpoint=data.endpoint)
+
+async def insert_site_stat(db: Session, data: schemas.SiteStat) -> tuple[schemas.Status, int]:
     try:
+        uuid = db.execute(Select(models.UUID).filter_by(id=data.uuid)).fetchone()
+        item = models.SiteStat(uuid=uuid, endpoint=data.endpoint)
         db.add(item)
         db.commit()
-    except:
-        return JSONResponse(content={"status": "OK"}, status_code=500)
-    return {"status": "OK"}
+    except SQLAlchemyError:
+        return schemas.Status(status="Nope"), 500
+    return schemas.Status(), 200
