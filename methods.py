59d78014e4559aa085cee5286eb651af7ff3e4d1
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-12-29 22:23:42 +0300

    Add new method for insering site statistics

--- methods.py
+++ methods.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
 import models, schemas
 
 async def create_uuid(db: Session) -> schemas.UUID:
@@ -7,3 +8,12 @@ async def create_uuid(db: Session) -> schemas.UUID:
     db.commit()
     db.refresh(item)
     return item
+
+async def insert_site_stat(db: Session, data: schemas.SiteStat) -> JSONResponse:
+    item = models.SiteStat(uuid=data.uuid, endpoint=data.endpoint)
+    try:
+        db.add(item)
+        db.commit()
+    except:
+        return JSONResponse(content={"status": "OK"}, status_code=500)
+    return {"status": "OK"}
