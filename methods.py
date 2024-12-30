5edef740b8efeb7f9c8d5571aefb9a782a2aa519
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-12-30 11:58:57 +0300

    New method for inserting selected auditory

--- methods.py
+++ methods.py
@@ -22,3 +22,15 @@ async def insert_site_stat(db: Session, data: schemas.SiteStat) -> tuple[schemas
     except SQLAlchemyError:
         return schemas.Status(status="Nope"), 500
     return schemas.Status(), 200
+
+
+async def insert_aud_selection(db: Session, data: schemas.SelectedAuditory) -> tuple[schemas.Status, int]:
+    try:
+        uuid = db.execute(Select(models.UUID).filter_by(id=data.uuid)).fetchone()
+        auditory = db.execute(Select(models.Auditory).filter_by(id=data.auditory)).fetchone()
+        item = models.SelectAuditory(uuid=uuid, auditory=auditory, success=data.success)
+        db.add(item)
+        db.commit()
+    except SQLAlchemyError:
+        return schemas.Status(status="Nope"), 500
+    return schemas.Status(), 200
