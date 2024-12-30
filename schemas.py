2a90d5ec443352a5b549cb11d0ff03a183e41922
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-12-30 12:53:33 +0300

    Rename field id in UserId structure

--- schemas.py
+++ schemas.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 
 class UserId(BaseModel):
-    id: str = Field(title="id",
-                    description="Unique user id",
-                    min_length=36,
-                    max_length=36,
-                    pattern=r"[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{8}")
+    user_id: str = Field(title="User-id",
+                         description="Unique user id",
+                         min_length=36,
+                         max_length=36,
+                         pattern=r"[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{8}")
     creation_date: datetime | None = Field(default=None)
     model_config = ConfigDict(from_attributes=True)
 
