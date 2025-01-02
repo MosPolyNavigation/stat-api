a1076410b235e5d700d5bfe4c260112d9ab128d0
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-02 16:24:40 +0300

    Fix models ID's and documentation for methods errors

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class SiteStat(Base):
     __tablename__ = "site_statistics"
 
     id: int = Column(Integer, primary_key=True, index=True)
-    user_id: str = Column(ForeignKey("user_ids.id"), nullable=False)
+    user_id: str = Column(ForeignKey("user_ids.user_id"), nullable=False)
     visit_date: datetime = Column(DateTime, default=datetime.now(), nullable=False)
     endpoint: str = Column(String(100), nullable=True)
 
@@ -36,7 +36,7 @@ class SelectAuditory(Base):
     __tablename__ = "selected_auditories"
 
     id: int = Column(Integer, primary_key=True, index=True)
-    user_id: str = Column(ForeignKey("user_ids.id"), nullable=False)
+    user_id: str = Column(ForeignKey("user_ids.user_id"), nullable=False)
     visit_date: datetime = Column(DateTime, default=datetime.now(), nullable=False)
     auditory_id: str = Column(ForeignKey("auditories.id"), nullable=False)
     success: bool = Column(Boolean, default=False, nullable=False)
