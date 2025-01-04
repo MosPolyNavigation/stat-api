c09b20be846e256952a3ef3048f71d9d95323ad0
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-04 18:12:14 +0300

    Add tests for select-aud and auds endpoints

---
+++ app/tests/test_auds.py
@@ -0,0 +1,45 @@
+from .base import client
+
+
+def test_get_auds():
+    response = client.get("/api/get/auds", params={
+        "api_key": "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+        "page": 1,
+        "size": 50
+    })
+    assert response.status_code == 200
+
+
+def test_user_404_stat_aud():
+    response = client.put("/api/stat/select-aud", json={
+        "user_id": "11e1a4b8-7fa7-4501-9faa-541a5e0ff1e1",
+        "auditory_id": "a-100",
+        "success": True
+    })
+    assert response.status_code == 404
+    assert response.json() == {"status": "User not found"}
+
+
+def test_auditory_404_stat_aud():
+    response = client.put("/api/stat/select-aud", json={
+        "user_id": "11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
+        "auditory_id": "a-12122",
+        "success": True
+    })
+    assert response.status_code == 404
+    assert response.json() == {"status": "Auditory not found"}
+
+
+def test_422_stat_aud():
+    response = client.put("/api/stat/select-aud", json={})
+    assert response.status_code == 422
+
+
+def test_stat_aud():
+    response = client.put("/api/stat/site", json={
+        "user_id": "11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
+        "auditory_id": "a-100",
+        "success": True
+    })
+    assert response.status_code == 200
+    assert response.json() == {"status": "OK"}
