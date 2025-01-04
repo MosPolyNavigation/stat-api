343a52b4dbcf7f474b7baeb89aeeb7b8399571c1
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-04 15:56:32 +0300

    Add new models for endpoint implementation

--- app/models.py
+++ app/models.py
@@ -60,3 +60,23 @@ class StartWay(Base):
     user: Mapped["UserId"] = relationship("UserId", back_populates="started_ways")
     start: Mapped["Auditory"] = relationship("Auditory", foreign_keys=[start_id])
     end: Mapped["Auditory"] = relationship("Auditory", foreign_keys=[end_id])
+
+
+class Plan(Base):
+    __tablename__ = "plans"
+
+    id: Mapped[str] = mapped_column(String(50), primary_key=True)
+
+    changed: Mapped[list["ChangePlan"]] = relationship("ChangePlan", back_populates="plan")
+
+
+class ChangePlan(Base):
+    __tablename__ = "changed_plans"
+
+    id: int = Column(Integer, primary_key=True, index=True)
+    user_id: str = Column(ForeignKey("user_ids.user_id"), nullable=False)
+    visit_date: datetime = Column(DateTime, default=datetime.now(), nullable=False)
+    plan_id: str = Column(ForeignKey("plans.id"), nullable=False)
+
+    user: Mapped["UserId"] = relationship("UserId", back_populates="selected")
+    plan: Mapped["Plan"] = relationship("Plan", back_populates="changed")
