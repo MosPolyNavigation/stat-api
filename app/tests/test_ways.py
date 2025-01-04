637c8e799b3ae6c07cca27781deba8a705ab4b07
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-04 18:19:18 +0300

    Add tests for start-way and ways endpoints

---
+++ app/tests/test_ways.py
@@ -0,0 +1,55 @@
+from .base import client
+
+
+def test_get_auds():
+    response = client.get("/api/get/ways", params={
+        "api_key": "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+        "page": 1,
+        "size": 50
+    })
+    assert response.status_code == 200
+
+
+def test_user_404_stat_ways():
+    response = client.put("/api/stat/start-way", json={
+        "user_id": "11e1a4b8-7fa7-4501-9faa-541a5e0ff1e1",
+        "start_id": "a-100",
+        "end_id": "a-101"
+    })
+    assert response.status_code == 404
+    assert response.json() == {"status": "User not found"}
+
+
+def test_start_404_stat_ways():
+    response = client.put("/api/stat/start-way", json={
+        "user_id": "11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
+        "start_id": "a-10011",
+        "end_id": "a-101",
+    })
+    assert response.status_code == 404
+    assert response.json() == {"status": "Start auditory not found"}
+
+
+def test_end_404_stat_ways():
+    response = client.put("/api/stat/start-way", json={
+        "user_id": "11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
+        "start_id": "a-100",
+        "end_id": "a-10111",
+    })
+    assert response.status_code == 404
+    assert response.json() == {"status": "End auditory not found"}
+
+
+def test_422_stat_aud():
+    response = client.put("/api/stat/start-way", json={})
+    assert response.status_code == 422
+
+
+def test_stat_aud():
+    response = client.put("/api/stat/start-way", json={
+        "user_id": "11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
+        "start_id": "a-100",
+        "end_id": "a-101",
+    })
+    assert response.status_code == 200
+    assert response.json() == {"status": "OK"}
