cd6acecdbb3d9c53621ae5b14eb854b9990bd988
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-05 22:23:29 +0300

    Add 422 tests for protected endpoints

--- app/tests/test_422.py
+++ app/tests/test_422.py
@@ -1,4 +1,4 @@
-"""Tests for 422 response for unprotected endpoints"""
+"""Tests for 422 response for unprotected and protected endpoints"""
 
 import pytest
 from .base import client
@@ -12,4 +12,19 @@ from .base import client
 ])
 def test_422_stat(endpoint):
     response = client.put(endpoint, json={})
+    assert response.status_code == 422
+
+
+@pytest.mark.parametrize("endpoint", [
+    "/api/get/site",
+    "/api/get/auds",
+    "/api/get/ways",
+    "/api/get/plans",
+    "/api/get/stat"
+])
+def test_422_get_protected(endpoint):
+    response = client.get(endpoint, params={
+        "api_key": "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+        "page": -1
+    })
     assert response.status_code == 422
