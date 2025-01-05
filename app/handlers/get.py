76b1b6a80a90e9007840462335a6ae5e3520c876
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-05 20:42:37 +0300

    add new method for gathering statistics

---
+++ app/handlers/get.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Select, and_, column, func
+from sqlalchemy.orm import Session
+from .filter import filter_by_date
+from app import schemas, models
+
+
+async def get_endpoint_stats(db: Session, params: schemas.FilterQuery):
+    query, borders = filter_by_date(params)
+    all_visits = len(db.execute(query).scalars().all())
+    unique_query = Select(func.count(models.UserId)).filter(column("user_id").in_(query))
+    if borders is not None:
+        unique_query = unique_query.filter(and_(
+            models.UserId.creation_date >= borders[0],
+            models.UserId.creation_date <= borders[1]
+        ))
+    unique_visitors = db.execute(unique_query).scalar()
+    return schemas.Statistics(
+        unique_visitors=unique_visitors,
+        all_visitors=all_visits-unique_visitors,
+        period=borders
+    )
