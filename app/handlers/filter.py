83d99d81854b75eb0782a1c1b71d7e4cef44929d
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-05 20:03:51 +0300

    Create method for date queries construction

--- app/handlers/filter.py
+++ app/handlers/filter.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Select
+from datetime import datetime, time
+from sqlalchemy import Select, and_
 from app import schemas
 from typing import Any
 
@@ -10,4 +11,22 @@ async def filter_by_user(
     query = Select(data_model)
     if params.user_id is not None:
         query = query.filter_by(user_id=params.user_id)
+    return query
+
+
+async def filter_by_date(params: schemas.FilterQuery) -> Select:
+    model = params.model
+    query = Select(model)
+    start_time = time(0, 0, 0)
+    end_time = time(23, 59, 59)
+    if params.start_date is not None and params.end_date is not None:
+        query = Select(model).filter(and_(
+            model.visit_date >= datetime.combine(params.start_date, start_time),
+            model.visit_date <= datetime.combine(params.end_date, end_time)
+        ))
+    elif params.start_date is not None and params.end_date is None:
+        query = query.filter(and_(
+            model.visit_date >= datetime.combine(params.start_date, start_time),
+            model.visit_date <= datetime.combine(params.start_date, end_time)
+        ))
     return query
