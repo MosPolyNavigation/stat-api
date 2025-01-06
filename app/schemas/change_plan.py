be13616b256721edafc08184830c71d0cecc3be5
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-06 04:09:00 +0300

    Create base classes, but without moving field to shared bases

--- app/schemas/change_plan.py
+++ app/schemas/change_plan.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, ConfigDict
 from datetime import datetime
 
 
-class ChangePlanIn(BaseModel):
+class ChangePlanBase(BaseModel):
     user_id: str = Field(title="id",
                          description="Unique user id",
                          min_length=36,
@@ -14,15 +14,10 @@ class ChangePlanIn(BaseModel):
                          min_length=3)
 
 
-class ChangePlanOut(BaseModel):
-    user_id: str = Field(title="id",
-                         description="Unique user id",
-                         min_length=36,
-                         max_length=36,
-                         pattern=r"[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{8}")
+class ChangePlanIn(ChangePlanBase):
+    pass
+
+
+class ChangePlanOut(ChangePlanBase):
     visit_date: datetime = Field(description="Date when user changed plan")
-    plan_id: str = Field(title="Changed-auditory",
-                         description="Changed plan by user",
-                         max_length=4,
-                         min_length=3)
     model_config = ConfigDict(from_attributes=True)
--- app/schemas/selected_aud.py
+++ app/schemas/selected_aud.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, ConfigDict
 from datetime import datetime
 
 
-class SelectedAuditoryIn(BaseModel):
+class SelectedAuditoryBase(BaseModel):
     user_id: str = Field(title="id",
                          description="Unique user id",
                          min_length=36,
@@ -16,17 +16,10 @@ class SelectedAuditoryIn(BaseModel):
                           description="Status of auditory selection")
 
 
-class SelectedAuditoryOut(BaseModel):
-    user_id: str = Field(title="id",
-                         description="Unique user id",
-                         min_length=36,
-                         max_length=36,
-                         pattern=r"[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{8}")
+class SelectedAuditoryIn(SelectedAuditoryBase):
+    pass
+
+
+class SelectedAuditoryOut(SelectedAuditoryBase):
     visit_date: datetime = Field(description="Date when user selected auditory")
-    auditory_id: str = Field(title="Selected-auditory",
-                             description="Selected auditory by user",
-                             max_length=50,
-                             min_length=1)
-    success: bool = Field(title="Selection-status",
-                          description="Status of auditory selection")
     model_config = ConfigDict(from_attributes=True)
