d4d703c87e6bb5806119554eb81b440d378cf4e8
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-15 22:30:49 +0300

    Add docs to schemas/statistics.py

--- app/schemas/statistics.py
+++ app/schemas/statistics.py
@@ -4,6 +4,17 @@ from typing import Optional
 
 
 class Statistics(BaseModel):
+    """
+    Класс для статистики.
+
+    Этот класс содержит поля, которые необходимы для статистики.
+
+    Attributes:
+        unique_visitors: Количество уникальных посетителей.
+        visitor_count: Количество посетителей.
+        all_visits: Общее количество посещений.
+        period: Период, за который собрана статистика.
+    """
     unique_visitors: int
     visitor_count: int
     all_visits: int
