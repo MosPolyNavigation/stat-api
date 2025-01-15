efb05cce34f949b6f082842657f5c118bad052af
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-15 23:01:38 +0300

    Add docs to all files in handlers

--- app/handlers/insert.py
+++ app/handlers/insert.py
@@ -5,7 +5,19 @@ from app.helpers.errors import LookupException
 
 
 async def insert_site_stat(db: Session, data: schemas.SiteStatIn) -> schemas.Status:
-    user = db.execute(Select(models.UserId).filter_by(user_id=data.user_id)).scalar_one_or_none()
+    """
+    Функция для добавления посещения сайта.
+
+    Эта функция добавляет посещение сайта в базу данных.
+
+    Args:
+        db: Сессия базы данных.
+        data: Данные посещения сайта.
+
+    Returns:
+        schemas.Status: Статус операции.
+    """
+    user = db.execute(Select(models.User).filter_by(user_id=data.user_id)).scalar_one_or_none()
     if user is None:
         raise LookupException("User")
     item = models.SiteStat(user=user, endpoint=data.endpoint)
@@ -15,7 +27,19 @@ async def insert_site_stat(db: Session, data: schemas.SiteStatIn) -> schemas.Sta
 
 
 async def insert_aud_selection(db: Session, data: schemas.SelectedAuditoryIn) -> schemas.Status:
-    user = db.execute(Select(models.UserId).filter_by(user_id=data.user_id)).scalar_one_or_none()
+    """
+    Функция для добавления выбранной аудитории.
+
+    Эта функция добавляет выбранную аудитории в базу данных.
+
+    Args:
+        db: Сессия базы данных.
+        data: Данные выбранной аудитории.
+
+    Returns:
+        schemas.Status: Статус операции.
+    """
+    user = db.execute(Select(models.User).filter_by(user_id=data.user_id)).scalar_one_or_none()
     auditory = db.execute(Select(models.Auditory).filter_by(id=data.auditory_id)).scalar_one_or_none()
     if user is None:
         raise LookupException("User")
@@ -28,7 +52,19 @@ async def insert_aud_selection(db: Session, data: schemas.SelectedAuditoryIn) ->
 
 
 async def insert_start_way(db: Session, data: schemas.StartWayIn) -> schemas.Status:
-    user = db.execute(Select(models.UserId).filter_by(user_id=data.user_id)).scalar_one_or_none()
+    """
+    Функция для добавления начатого пути.
+
+    Эта функция добавляет начатый пути в базу данных.
+
+    Args:
+        db: Сессия базы данных.
+        data: Данные начатого пути.
+
+    Returns:
+        schemas.Status: Статус операции.
+    """
+    user = db.execute(Select(models.User).filter_by(user_id=data.user_id)).scalar_one_or_none()
     start = db.execute(Select(models.Auditory).filter_by(id=data.start_id)).scalar_one_or_none()
     end = db.execute(Select(models.Auditory).filter_by(id=data.end_id)).scalar_one_or_none()
     if user is None:
@@ -44,7 +80,19 @@ async def insert_start_way(db: Session, data: schemas.StartWayIn) -> schemas.Sta
 
 
 async def insert_changed_plan(db: Session, data: schemas.ChangePlanIn) -> schemas.Status:
-    user = db.execute(Select(models.UserId).filter_by(user_id=data.user_id)).scalar_one_or_none()
+    """
+    Функция для добавления смененного плана.
+
+    Эта функция добавляет смененный план в базу данных.
+
+    Args:
+        db: Сессия базы данных.
+        data: Данные смененный плана.
+
+    Returns:
+        schemas.Status: Статус операции.
+    """
+    user = db.execute(Select(models.User).filter_by(user_id=data.user_id)).scalar_one_or_none()
     plan = db.execute(Select(models.Plan).filter_by(id=data.plan_id)).scalar_one_or_none()
     if user is None:
         raise LookupException("User")
