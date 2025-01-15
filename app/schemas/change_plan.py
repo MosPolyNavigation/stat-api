e93b4f2aa8da603bec399858ad10afafbf123d22
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-15 21:38:04 +0300

    Add docs to schemas/change_plan.py

--- app/schemas/change_plan.py
+++ app/schemas/change_plan.py
@@ -3,6 +3,15 @@ from datetime import datetime
 
 
 class ChangePlanBase(BaseModel):
+    """
+    Базовый класс для изменения плана.
+
+    Этот класс содержит поля, которые необходимы для изменения плана.
+
+    Attributes:
+        user_id: Уникальный идентификатор пользователя.
+        plan_id: Идентификатор измененного плана.
+    """
     user_id: str = Field(title="id",
                          description="Unique user id",
                          min_length=36,
@@ -15,9 +24,22 @@ class ChangePlanBase(BaseModel):
 
 
 class ChangePlanIn(ChangePlanBase):
+    """
+    Класс для входных данных изменения плана.
+
+    Этот класс наследуется от ChangePlanBase и не содержит дополнительных полей.
+    """
     pass
 
 
 class ChangePlanOut(ChangePlanBase):
+    """
+    Класс для выходных данных изменения плана.
+
+    Этот класс наследуется от ChangePlanBase и содержит дополнительное поле visit_date.
+
+    Attributes:
+        visit_date: Дата, когда пользователь изменил план.
+    """
     visit_date: datetime = Field(description="Date when user changed plan")
     model_config = ConfigDict(from_attributes=True)
