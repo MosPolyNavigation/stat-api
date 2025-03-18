988bd5a3701d7ef8aea633791aa85e2a20d57899
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-03-18 21:09:17 +0300

    Add docs to image endpoint

--- app/routes/review.py
+++ app/routes/review.py
@@ -134,7 +134,50 @@ async def get_reviews(
     return paginate(db, filter_by_user(models.Review, query))
 
 
-@router.get("/image/{image_path}")
+@router.get(
+    "/image/{image_path}",
+    description="Эндпоинт для получения картинок из отзывов",
+    response_class=FileResponse,
+    tags=["review"],
+    responses={
+        500: {
+            'model': status.Status,
+            'description': "Server side error",
+            'content': {
+                "application/json": {
+                    "example": {"status": "Some error"}
+                }
+            }
+        },
+        404: {
+            'model': status.Status,
+            'description': "Item not found",
+            'content': {
+                "application/json": {
+                    "example": {"status": "Image not found"}
+                }
+            }
+        },
+        200: {
+            'content': {
+                "image/png": {
+                    "schema": {
+                        "type": "string",
+                        "format": "binary"
+                    }
+                },
+                "image/jpeg": {
+                    "schema": {
+                        "type": "string",
+                        "format": "binary"
+                    }
+                },
+            },
+            "description": "Review image",
+
+        }
+    }
+)
 async def get_image(
         image_path: str
 ) -> FileResponse:
