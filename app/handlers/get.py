6e11295ee43e095ca6fba07b5d7f65a5eb1c7a93
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-03-19 18:10:53 +0300

    Add handler for popular auditories gathering

--- app/handlers/get.py
+++ app/handlers/get.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Select, and_, column, func
-from sqlalchemy.orm import Session
+from sqlalchemy import Select, and_, column, func, union_all
+from sqlalchemy.orm import Session, aliased
+
 from .filter import filter_by_date
 from app import schemas, models
 
@@ -33,3 +34,50 @@ async def get_endpoint_stats(db: Session, params: schemas.FilterQuery):
         all_visits=all_visits,
         period=borders
     )
+
+def get_popular_auds_query():
+    """
+        Query in basis:
+        ```sql
+            SELECT ID from
+              (SELECT auditory_id as ID, count(*) as CNT from selected_auditories where success=1 group by auditory_id UNION ALL
+              SELECT start_id as ID, count(*)*3 as CNT from started_ways group by start_id UNION ALL
+              SELECT end_id as ID, count(*)*3 as CNT from started_ways group by end_id) as tr
+            GROUP BY ID
+            ORDER BY SUM(CNT) DESC;
+        ```
+    """
+    return aliased(
+        union_all(
+            Select(
+                models.SelectAuditory.auditory_id.label('ID'),
+                func.count().label('CNT'))
+            .select_from(models.SelectAuditory)
+            .filter_by(success=True)
+            .group_by(models.SelectAuditory.auditory_id.label('ID')),
+            Select(models.StartWay.start_id.label('ID'),
+                   func.count().label('CNT') * 3)
+            .select_from(models.StartWay)
+            .group_by(models.StartWay.start_id.label('ID')),
+            Select(models.StartWay.end_id.label('ID'),
+                   func.count().label('CNT') * 3)
+            .select_from(models.StartWay)
+            .group_by(models.StartWay.end_id.label('ID'))
+        ).alias('tr')
+    )
+
+async def get_popular_auds(db: Session):
+    tr = get_popular_auds_query()
+    query = (Select(tr.c.ID)
+             .select_from(tr)
+             .group_by(tr.c.ID)
+             .order_by(func.sum(tr.c.CNT).desc()))
+    return db.execute(query).scalars().all()
+
+async def get_popular_auds_with_count(db: Session):
+    tr = get_popular_auds_query()
+    query = (Select(tr.c.ID, func.sum(tr.c.CNT))
+             .select_from(tr)
+             .group_by(tr.c.ID)
+             .order_by(func.sum(tr.c.CNT).desc()))
+    return db.execute(query).fetchall()
