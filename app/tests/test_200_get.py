f884045839b26553aa50d1a087b91f4d12e5094e
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-04-15 20:08:31 +0300

    Add tests for route finder

--- app/tests/test_200_get.py
+++ app/tests/test_200_get.py
@@ -1,5 +1,8 @@
 import re
+import pickle
 import pytest
+import app.globals as globals_
+from .load_graph import graph_b
 from .base import client
 
 
@@ -36,3 +39,18 @@ def test_get_user_id():
         r"[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{8}",
         response.json()["user_id"]
     ) is not None
+
+
+def test_get_popular():
+    response = client.get("/api/get/popular")
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+
+
+def test_get_route():
+    globals_.global_graph["BS"] = pickle.loads(graph_b)
+    response = client.get("/api/get/route?from_=a-100&to=a-101")
+    assert response.status_code == 200
+    json_data = response.json()
+    assert len(json_data["steps"]) == 1
+    assert json_data["fullDistance"] == 855
