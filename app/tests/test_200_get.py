7e094a02960203efab3728e6e2c5186f04feff84
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-10-13 14:41:29 +0300

    Удалить неактуальный тест

--- app/tests/test_200_get.py
+++ app/tests/test_200_get.py
@@ -17,15 +17,6 @@ def test_200_get(endpoint):
     assert response.status_code == 200
 
 
-@pytest.mark.parametrize("target", ["site", "auds", "ways", "plans"])
-def test_get_stat(target):
-    response = client.get("/api/get/stat", params={
-        "target": "site"
-    }, headers={"Authorization": "Bearer 11e1a4b8-7fa7-4501-9faa-541a5e0ff1ed"})
-    assert response.status_code == 200
-    assert response.json()["unique_visitors"] == 1
-
-
 def test_get_user_id():
     response = client.get("/api/get/user-id")
     assert response.status_code == 200
