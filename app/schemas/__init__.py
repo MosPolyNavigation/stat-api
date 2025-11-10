108615d9f7b06813ffc4761eb796d05f6c11d760
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-11-10 23:16:20 +0300

    Выделил схемы статистики в отдельный модуль

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,12 +1,12 @@
-from .change_plan import ChangePlanIn
+from app.schemas.stat.change_plan import ChangePlanIn
 from .filter import Filter, FilterQuery, FilterRoute
-from .selected_aud import SelectedAuditoryIn
-from .site_stat import SiteStatIn
-from .start_way import StartWayIn
+from app.schemas.stat.selected_aud import SelectedAuditoryIn
+from app.schemas.stat.site_stat import SiteStatIn
+from app.schemas.stat.start_way import StartWayIn
 from .statistics import Statistics
 from .status import Status
 from .review import Problem
-from .user_id import UserId, UserIdCheck
+from app.schemas.stat.user_id import UserId, UserIdCheck
 from app.schemas.graph.dto import DataDto, GraphDto, LocationDto, \
     CorpusDto, PlanDto, RoomDto, NearestDto
 from app.schemas.graph.data import LocationData, CorpusData, PlanData
