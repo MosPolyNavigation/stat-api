180cd3f24b06cd39071fc3ebcab7035ce3780ddd
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-11-20 20:07:21 +0300

    Успешно добавлен функционал получения расписания

---
+++ app/helpers/rasp/get_schedule.py
@@ -0,0 +1,64 @@
+import asyncio
+
+import httpx
+import json
+import re
+from pydantic import ValidationError
+from typing import Any, Union, AsyncGenerator
+from app.schemas.rasp.dto import Dto
+
+BASE_URL = "https://rasp.dmami.ru/"
+dataUrl = f'{BASE_URL}site/group?group='
+DEFAULT_DELAY = 0.1
+USER_AGENT = 'Mozilla/5.0 (compatible; schedule-extractor/1.2; +https://github.com/MosPolyNavigation/stat-api)'
+
+
+def extract_json_string(html: str) -> str:
+    """
+    Извлекает JSON-объект, присвоенный переменной globalListGroups.
+    """
+    pattern = r'\b(?:var|let|const)\s+globalListGroups\s*=\s*({.*?});'
+    match = re.search(pattern, html, re.DOTALL)
+    if not match:
+        raise ValueError('Не удалось найти globalListGroups в HTML.')
+
+    json_str = match.group(1).split("\n")[0].rstrip('.groups;')
+    if json_str.count('{') != json_str.count('}'):
+        raise ValueError('Несбалансированные фигурные скобки в JSON.')
+    return json_str
+
+
+async def get_groups() -> dict[str, dict[str, Any | None] | Any]:
+    async with httpx.AsyncClient(timeout=30) as client:
+        r = await client.get(BASE_URL, headers={'User-Agent': USER_AGENT})
+        r.raise_for_status()
+    html = r.text
+    json_str = extract_json_string(html)
+    data = json.loads(json_str)
+    return data
+
+
+async def get_schedule() -> AsyncGenerator[tuple[str, Union[Dto, None]], None]:
+    groups = (await get_groups())['groups']
+    for key, value in groups.items():
+        if value:
+            yield key, None
+            continue
+        async with httpx.AsyncClient(timeout=30) as client:
+            r = await client.get(
+                f'{dataUrl}{key}',
+                headers={'User-Agent': USER_AGENT, "Referer": "https://rasp.dmami.ru"})
+            if r.status_code != 200:
+                yield key, None
+                continue
+            json_obj = r.json()
+            try:
+                dto = Dto(**json_obj)
+                yield key, dto
+            except ValidationError:
+                print(
+                    f"Ошибка парсинга расписания для группы {key}",
+                    f"из-за ошибки получения расписания: {json_obj['message']}"
+                )
+                yield key, None
+            await asyncio.sleep(DEFAULT_DELAY)
---
+++ app/schemas/rasp/dto.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+from typing import Union, Any
+
+
+class AuditoryDto(BaseModel):
+    title: str
+    color: str
+
+
+class LessonDto(BaseModel):
+    sbj: str
+    teacher: str
+    dts: str
+    df: str
+    dt: str
+    auditories: list[AuditoryDto]
+    shortRooms: list[str]
+    location: str
+    type: str
+    week: str
+    align: str
+    e_link: Union[Any, None]
+
+
+class GroupDto(BaseModel):
+    title: str
+    course: int
+    dateFrom: str
+    dateTo: str
+    evening: int
+    comment: str
+
+
+class Dto(BaseModel):
+    status: str
+    grid: dict[str, dict[str, list[LessonDto]]]
+    group: GroupDto
+    isSession: bool
