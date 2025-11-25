f242c9b89a846b71b10f2b08755caa98a30a50af
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-11-25 18:32:25 +0300

    Подключил созданные эндпоинты в приложение

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from .config import get_settings
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
 from sqlalchemy.exc import SQLAlchemyError
-from .routes import get, stat, review, check, auth, graphql, crud_users, crud_roles, jobs
+from .routes import get, stat, review, check, auth, graphql, crud_users, crud_roles, jobs, free_aud
 from fastapi import FastAPI, Request, HTTPException
 from .state import AppState
 from os import path, makedirs
@@ -35,6 +35,10 @@ tags_metadata = [
     {
         "name": "jobs",
         "description": "Эндпоинты для управления фоновыми задачами"
+    },
+    {
+        "name": "free-aud",
+        "description": "Эндпоинты для получения свободных аудиторий"
     }
 ]
 
@@ -71,6 +75,7 @@ app.include_router(graphql.graphql_router, prefix="/api/graphql", tags=["graphql
 app.include_router(crud_users.router)
 app.include_router(crud_roles.router)
 app.include_router(jobs.router)
+app.include_router(free_aud.router)
 app.mount(
     "/admin/",
     StaticFiles(
