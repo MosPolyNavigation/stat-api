3ab1248203318de1958ca8cb3a54f75bab0ce235
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2026-04-08 20:40:12 +0300

    Исправил ошибку в миграциях И обновил lock файл

--- alembic/versions/db677cf8a2ba_authentication_jwt_access_refresh_tokens.py
+++ alembic/versions/db677cf8a2ba_authentication_jwt_access_refresh_tokens.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'db677cf8a2ba'
-down_revision: Union[str, None] = '1fa1ab15c6db'
+down_revision: Union[str, None] = '8ae79c53139e'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
