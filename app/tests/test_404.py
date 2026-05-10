867da38747087de5cc1d07fa4f08433c50303e32
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2026-05-11 00:34:15 +0300

    [Broken state] Этот тест случайно затронул

--- app/tests/test_404.py
+++ app/tests/test_404.py
@@ -30,11 +30,10 @@ def test_400_stat_event_payload_not_allowed():
         json={
             "ident": "22e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
             "event_type_id": EVENT_TYPE_AUDS_ID,
-            "payloads": {str(PAYLOAD_TYPE_ENDPOINT_ID): "/api/get/route"},
+            "payloads": {PAYLOAD_TYPE_ENDPOINT_ID: "/api/get/route"},
         },
     )
     assert response.status_code == 400
-    print(response.text)
     assert response.json() == {
         "detail": "Payload type 1 is not allowed for event_type_id=2"
     }
